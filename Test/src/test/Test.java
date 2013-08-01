aaf21cc10f8fbe7b926e29e2deb2f7249d95d4ab
sakostomarov <sakostomarov@gmail.com>
2013-08-01 19:25:26 +0400

    test commit

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -15,5 +15,7 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world");
+        
     }
 }
